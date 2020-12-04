c8e88977a26c7405c454f21fea8f2b3a842196dc
hackorKR <hackorkr@gmail.com>
2020-12-04 17:04:55 +0900

    change max-width

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import axios from 'axios';
 
 const httpClient = axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
-  params: {key: process.env.REACT_APP_YOUTUBE_API_KEY},
+  params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
 });
 
 const youtube = new Youtube(httpClient);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App youtube={youtube}/>
+    <App youtube={youtube} />
   </React.StrictMode>,
   document.getElementById('root')
 );
